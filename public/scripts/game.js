7965db367cbeb31ac4ae2c62f00c5f0b730c4e11
sepseel <sepsel64@gmail.com>
2020-02-13 10:57:10 +0100

    moved drawing to own file

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -80,56 +80,7 @@ function draw() {
 
 // TODO move drawing functions to own file
 
-function drawRocket(rocket) {
-  rectMode(CENTER);
-  imageMode(CENTER);
-  if (rocket.id === id) {
-    stroke(0, 255, 0);
-  } else {
-    stroke(255);
-  }
-  push();
-  translate(rocket.coords.x, rocket.coords.y);
-  rotate(rocket.angle+(Math.PI/2));
-  scale(0.3);
-  if (rocket.thrusting) {
-    image(rocketAccelerateImg, 0, 0);
-  } else {
-    image(rocketImg, 0, 0);
-  }
-  pop();
-}
 
-function drawBullet(bullet) {
-  ellipseMode(CENTER);
-  fill("red")
-  noStroke()
-  push();
-  translate(bullet.coords.x, bullet.coords.y);
-  ellipse(0, 0, 5);
-  pop();
-}
-
-function drawPowerUp(powerup) {
-  ellipseMode(CENTER);
-  fill("green")
-  noStroke()
-  push();
-  translate(powerup.coords.x, powerup.coords.y);
-  ellipse(0, 0, 15);
-  pop();
-}
-
-function drawAmmo() {
-  ellipseMode(CENTER);
-  fill("red")
-  noStroke()
-  coords = {x: 10, y: 10};
-  for (let i = 0; i < ammo; i++) {
-    ellipse(coords.x, coords.y, 8);
-    coords.x += 13;
-  }
-}
 
 function keyPressed() {
   let newInput = false;
