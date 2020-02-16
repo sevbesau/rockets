b3840b9c40030d0da008d5dfa3bf3a092a55693e
sepseel <sepsel64@gmail.com>
2020-02-16 22:41:19 +0100

    use database

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const passport = require('passport');
+const database = require('../bin/database');
+const { sanitize } = require('../bin/util');
 
 const router = express.Router();
-
-// DATABASE !!!!!!!!!!
-
-let users = []; // TODO move into database!!
-// TODO sanitize form inputs!
 
 const initializePassport = require('../bin/passport-config');
 
 // set up passport 
 initializePassport(
   passport, 
-  // TODO replace with database querrys
-  email => users.find(user => user.email === email),
-  id => users.find(user => user.id === id)
+  email => database.getUserByEmail(email),
+  id => database.getUserById(id)
 );
 
 // route to get the login page
@@ -36,17 +32,22 @@ router.post('/login', passport.authenticate('local', {
   failureFlash: true
 }));
 
-// route for the register form
-router.post('/register', async (req, res) => {
+/**
+ * Check if all the fields contain the expected data types and formats
+ * @param {*} req the post request containg the user data
+ */
+function validateRegisterForm(req) {
+  // VALIDATE email
   // VALIDATE does email exist in database?
-
-  const { name, email, password, password2 } = req.body;
   let errors = [];
 
-  console.log(name)
   // are all the fields filled in?
   if (!name || !email || !password || !password2) {
     errors.push({ msg: 'Please fill out all fields.' });
+  }
+  // do any of the fields contain an ; ?
+  if (name.contains(";") && email.contains(";") || password.contains(";")) {
+    errors.push({msg: "Please dont use ';'"});
   }
   // is the password long enough?
   if (password.length < 6) {
@@ -56,29 +57,28 @@ router.post('/register', async (req, res) => {
   if (password !== password2) {
     errors.push({ msg: 'Passwords must match.'})
   }
+  return errors;
+}
 
+// route for the register form
+router.post('/register', async (req, res) => {
+  const errors = validateRegisterForm(req);
+  const { name, email, password, password2 } = req.body;
   if (errors.length > 0) {
-    // TODO pass filled in fields to re render, so they dont get lost
-    res.render('register', { errors, name, email, password, password2 })
+    // invalid form
+    res.render('register', { errors, username, email, password, password2 })
   } else {
-    // validation passed
+    // valid form
     try {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10)
-      users.push({
-        id: Date.now().toString(),
-        name: req.body.name, 
-        email: req.body.email,
-        password: hashedPassword
-      })
+      const hashedPassword = await bcrypt.hash(password, 10)
+      database.putUser(email, username, hashedPassword);
       req.flash('success_msg', 'You are now registered and can log in')
       res.redirect('/users/login');
     } catch {
       errors.push({ msg: "Oops, something went wrong, please try again."})
-      res.render('register', { errors, name, email, password, password2 })
+      res.render('register', { errors, username, email, password, password2 })
     }
   }
-
-  
 })
 
 module.exports = router;
