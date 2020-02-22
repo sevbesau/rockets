241d434db0d92d2510e0a19b9d7872c778a142d8
sepseel <sepsel64@gmail.com>
2020-02-23 00:27:32 +0100

    added more models

---
+++ models/game.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require('sequelize');
+const dBconnection = require('./databaseConnection');
+
+module.exports = dBconnection.define('Game', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true
+  },
+  title: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false
+  },
+}, {
+  timestamps: false
+})
+ 
---
+++ models/score.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require('sequelize');
+const dBconnection = require('./databaseConnection');
+
+module.exports = dBconnection.define('Score', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true
+  },
+  score: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+}, {
+  timestamps: false
+})
+ 
